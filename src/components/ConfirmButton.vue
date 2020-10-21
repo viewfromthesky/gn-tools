f69545a05a3d971d618b421478bf24248239d8aa
Tom Sykes <tom@macbook-pro.local>
2020-10-21 08:16:01 +0000

    Adding confirm button

---
+++ src/components/ConfirmButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <input v-if="!confirm" :class="classes" type="button" :value="text" @click="startConfirm" />
+  <input v-else :class="classes" type="button" @click="confirmed" value="Confirm" />
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        confirm: false
+      }
+    },
+    props: {
+      text: {
+        type: String,
+        default: ''
+      },
+      classes: {
+        type: Array,
+        default: () => ([])
+      }
+    },
+    methods: {
+      startConfirm() {
+        this.confirm = true;
+        setTimeout(() => {
+          this.confirm = false;
+        }, 3000);
+      },
+      confirmed() {
+        this.$emit('confirmed');
+        this.confirm = false;
+      }
+    }
+  };
+</script>
